5596bd1c50a61e5735219033e7da5179cfd1c9e0
arifmunir77 <91951862+reactDev777@users.noreply.github.com>
2025-12-23 22:36:03 +0500

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ const allowedOrigins = [
   "http://localhost:3000",   // React dev
   "http://localhost:5173",   // sometimes React dev runs on this
   "https://carelink.example.com", // production frontend
+  "https://olo-backend.onrender.com"
 ];
 
 const corsOptions: cors.CorsOptions = {
